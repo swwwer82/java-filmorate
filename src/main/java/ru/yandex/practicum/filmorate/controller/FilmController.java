fdda675d44da294ed4ffbec410d96a30614e9b9d
swwwer82 <swwwer82@yandex.ru>
2024-02-09 13:33:56 +0300

    update film controller

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,61 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.util.FilmSequence;
+
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static ru.yandex.practicum.filmorate.exception.CustomExceptions.FilmException;
 
 @RestController
+@RequestMapping(value = "/films")
+@Slf4j
 public class FilmController {
+    private final Map<Long, Film> idToFilms = new HashMap<>();
+
+    @PostMapping
+    public Film add(@Valid @RequestBody Film film) {
+        long id = FilmSequence.getNextId();
+
+        if (film.getId() == null) {
+            film.setId(id);
+        }
+
+        if (idToFilms.containsKey(film.getId())) {
+            log.error("Фильм с id={} уже существует", film.getId());
+            throw new FilmException(String.format("Пользователь с id=%s уже существует", film.getId()));
+        }
+
+        idToFilms.put(film.getId(), film);
+        log.info("Сохраняем новый фильм {}", film);
+
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@Valid @RequestBody Film film) {
+        if (!idToFilms.containsKey(film.getId())) {
+            log.error("Фильм с id={} не существует", film.getId());
+            throw new FilmException(String.format("Фильм с id=%s не существует", film.getId()));
+        }
+
+        idToFilms.put(film.getId(), film);
+        log.info("Обновляем фильм {}", film);
+
+        return film;
+    }
+
+    @GetMapping
+    public List<Film> findAll() {
+        log.info("Возвращаем все фильмы. Общее количество: {}", idToFilms.size());
+
+        return new ArrayList<>(idToFilms.values());
+    }
 }
