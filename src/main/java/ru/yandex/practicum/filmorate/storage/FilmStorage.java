2af3f7916fe1e09a0fab34fbc88594a7dcb98c86
swwwer82 <swwwer82@yandex.ru>
2024-02-27 21:27:10 +0300

    add : new interfaces

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import java.util.List;
+import ru.yandex.practicum.filmorate.model.Film;
+
+public interface FilmStorage {
+    Film add(Film film);
+
+    Film update(Film film);
+
+    void remove(Long id);
+
+    List<Film> findAll();
+
+    Film findById(Long id);
+
+    long getNextId();
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import java.util.List;
+import ru.yandex.practicum.filmorate.model.User;
+
+public interface UserStorage {
+    User add(User user);
+
+    User update(User user);
+
+    void remove(Long id);
+
+    List<User> findAll();
+
+    User findById(Long id);
+
+    long getNextId();
+}
