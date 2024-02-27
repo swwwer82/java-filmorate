1d940c6605b64f227c019fe3ab73770425b7b2e8
swwwer82 <swwwer82@yandex.ru>
2024-02-27 21:49:00 +0300

    add : new class

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -0,0 +1,46 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Film;
+
+@Component
+public class InMemoryFilmStorage implements FilmStorage {
+    private static long id = 1;
+    private final Map<Long, Film> idToFilm = new HashMap<>();
+
+    @Override
+    public Film add(Film film) {
+        idToFilm.put(film.getId(), film);
+        return film;
+    }
+
+    @Override
+    public Film update(Film film) {
+        idToFilm.put(film.getId(), film);
+        return film;
+    }
+
+    @Override
+    public void remove(Long id) {
+        idToFilm.remove(id);
+    }
+
+    @Override
+    public List<Film> findAll() {
+        return new ArrayList<>(idToFilm.values());
+    }
+
+    @Override
+    public Film findById(Long id) {
+        return idToFilm.get(id);
+    }
+
+    @Override
+    public long getNextId() {
+        return id++;
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -0,0 +1,49 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.User;
+
+@Component
+@Slf4j
+public class InMemoryUserStorage implements UserStorage {
+    private static long id = 1;
+    private final Map<Long, User> idToUser = new HashMap<>();
+
+    @Override
+    public User add(User user) {
+        idToUser.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User update(User user) {
+        idToUser.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public void remove(Long id) {
+        idToUser.remove(id);
+    }
+
+    @Override
+    public List<User> findAll() {
+
+        return new ArrayList<>(idToUser.values());
+    }
+
+    @Override
+    public User findById(Long id) {
+        return idToUser.get(id);
+    }
+
+    @Override
+    public long getNextId() {
+        return id++;
+    }
+}
