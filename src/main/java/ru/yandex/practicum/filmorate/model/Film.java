ad465489652a9a51cf000496bf3ca79fc2a06207
swwwer82 <swwwer82@yandex.ru>
2024-03-16 14:34:00 +0300

    fix : recommend reviewer

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import ru.yandex.practicum.filmorate.annotation.ValidReleaseDate;
